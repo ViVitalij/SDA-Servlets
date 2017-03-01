df81ad502ce4eed41e8af6b68b591363ae9f328d
Vitalij <kontrabasista@wp.pl>
2017-03-01 19:10:04 +0100

    Added UserServlet

---
+++ src/main/java/pl/sda/User.java
@@ -0,0 +1,22 @@
+package pl.sda;
+
+/**
+ * Created by m.losK on 2017-03-01.
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
